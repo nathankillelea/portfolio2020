fa759135b78af8d4b4bb8b85fa87379fb9f80d51
nathankillelea <nathan.killelea@gmail.com>
2020-09-15 05:08:25 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Card from './Card.js';
 import './App.css';
 import scrollToComponent from 'react-scroll-to-component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faFileAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 class App extends Component {
   render() {
@@ -13,8 +17,8 @@ class App extends Component {
           </div>
           <nav>
             <ul>
-              <li><div><p className='nav-text' onClick={() => scrollToComponent(this.Projects, {offset: -102, align: 'top', duration: 750})}>Projects</p></div></li>
-              <li><div><p className='nav-text' onClick={() => scrollToComponent(this.Contact, {offset: 0, align: 'top', duration: 1500})}>Contact</p></div></li>
+              <li className='nav-item-margin-right'><p className='nav-text' onClick={() => scrollToComponent(this.Projects, {offset: -102, align: 'top', duration: 750})}>Projects</p></li>
+              <li className='nav-item'><p className='nav-text' onClick={() => scrollToComponent(this.Contact, {offset: 0, align: 'top', duration: 1500})}>Contact</p></li>
             </ul>
           </nav>
         </header>
@@ -66,21 +70,43 @@ class App extends Component {
           />
         </section>
         <section id='about'>
-          <h1>Education</h1>
-          <ul>
-            <li><p className='years'>2014 - 2016</p></li>
-            <li><p>Illinois Valley Community College</p></li>
-            <li><p className='years'>2016 - 2018</p></li>
-            <li><p>University of Illinois at Urbana-Champaign</p></li>
-          </ul>
-          <h1>Work</h1>
-          <ul>
-            <li><p className='years'>2018 - 2019</p></li>
-            <li><p>Epic Systems</p></li>
-          </ul>
-          <h1>Contact</h1>
+          <section className='about-section' id='education'>
+            <h1 className='about-title'>Education</h1>
+            <ul>
+              <div className='about-item-margin-bottom'>
+                <li><p className='years'>2014 - 2016</p></li>
+                <li><p>A.E.S. / IVCC</p></li>
+              </div>
+              <div className='about-item'>
+                <li><p className='years'>2016 - 2018</p></li>
+                <li><p>B.S. Computer Science / UIUC</p></li>
+              </div>
+            </ul>
+          </section>
+          <section className='about-section-margin-top' id='work'>
+            <div>
+              <h1 className='about-title'>Work</h1>
+              <ul>
+                <div className='about-item'>
+                  <li><p className='years'>2018 - 2019</p></li>
+                  <li><p>Epic Systems</p></li>
+                </div>
+              </ul>
+            </div>
+          </section>
+          <section className='about-section-margin-top' id='contact'>
+            <div>
+              <h1 className='about-title'>Contact</h1>
+              <ul id='contact-group'>
+                <li><a href='https://github.com/nathankillelea' target='_blank' rel='noopener noreferrer' className='icon-bubble margin-right'><FontAwesomeIcon className='icon' icon={faGithubAlt} /></a></li>
+                <li><a href='https://www.linkedin.com/in/killelea/' target='_blank' rel='noopener noreferrer' className='icon-bubble margin-right'><FontAwesomeIcon className='icon' icon={faLinkedinIn} /></a></li>
+                <li><a href='mailto:nathan.killelea@gmail.com' className='icon-bubble margin-right'><FontAwesomeIcon className='icon' icon={faEnvelope} /></a></li>
+                <li><a href='' className='icon-bubble'><FontAwesomeIcon className='icon' icon={faFileAlt} /></a></li>
+              </ul>
+            </div>
+          </section>
         </section>
-        <section id='contact' ref={(section) => {this.Contact = section;}}>
+        <section id='contact-old' ref={(section) => {this.Contact = section;}}>
           <h1 id='contact-text'>Contact me!</h1>
         </section>
       </div>
--- src/Card.js
+++ src/Card.js
@@ -9,25 +9,6 @@ class Card extends Component {
         <div className="top-card" style={{'background': this.props.backgroundColor}}>
           <img src={this.props.img} />
         </div>
-        <div className="bottom-card">
-          <div className="info-text">
-            <h1 id="project-title">{this.props.title}</h1>
-            <div className="info-text-bottom">
-              <p id="description">{this.props.description}</p>
-              <div className="tags">
-              {this.props.tags.map((tag, index) => {
-                return <p className="tag" key={index}>{tag}</p>
-              })}
-              </div>
-              <div className="links">
-                <a href={this.props.sourceLink} id="source-link">
-                  <i className="fa fa-github fa" style={{color:'#fff'}}></i>
-                  Source Code
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     );
   }
