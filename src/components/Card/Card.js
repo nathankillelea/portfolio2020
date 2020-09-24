4f774c43b6668399dc93a959086fbbbdf6cd331c
nathankillelea <nathan.killelea@gmail.com>
2020-09-24 15:46:55 -0500

    card anim progress

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,25 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { useSpring, animated, config } from 'react-spring';
+import { useSpring, animated, config, useChain } from 'react-spring';
 import styles from './Card.module.css';
 
 const Card = (props) => {
   const [hover, setHover] = useState(false);
+  //const hover1Ref = useRef();
   const hoverState = useSpring({
     transform: hover
-      ? 'translate3d(0px, -12px, 0px) scale(1.01)'
-      : 'translate3d(0px, 0px, 0px) scale(1)',
-    config: config.wobbly
+      ? 'scale(1.02)'
+      : 'scale(1)',
+      config: config.wobbly
+      /*ref: hover1Ref*/
   });
 
-  return (
+  /*
+  const hover2Ref = useRef();
+  const hoverState2 = useSpring({
+    transform: hover
+    ? 'scale(1.06)'
+    : 'scale(1)',
+    ref: hover2Ref
+  });
+  */
+
+  //const color1Ref = useRef();
+  const color = useSpring({
+    from: {position: 'absolute', top: 0, left: 0, width: '0%', height: '100%', backgroundColor: `${props.backgroundColor}`},
+    to: hover ? {position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: `${props.backgroundColor}`} : null,
+    /*ref: color1Ref*/
+  });
+
+  //const color2Ref = useRef();
+  const color2 = useSpring({
+    from: {position: 'absolute', top: 0, right: 0, width: '100%', height: '100%', backgroundColor: 'rgba(28,29,37, 0.4)'},
+    to: hover ? {position: 'absolute', top: 0, right: 0, width: '0%', height: '100%', backgroundColor: 'rgba(28,29,37, 0.4)'} : null,
+    /*ref: color2Ref*/
+  });
+
+  //useChain([color1Ref, hover2Ref]);
+
+  // make words scale in size after
+  // make this better, not very good rn, text feels weird
+
+  return ( 
     <Link to={props.link} className={styles.card}>
       <animated.div
-        style={{...hoverState}}
+        style={hoverState}
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
       >
-        <img id={styles.img} src={`${props.img}`}/>
+        <animated.div style={color} />
+        <animated.div style={color2} />
+        <div id={styles.textContainer}>
+          <h1 id={styles.title}>{props.title}</h1>
+          <h2 id={styles.subtitle}>{props.description}</h2>
+        </div>
+        <img id={styles.img} src={props.img}/>
       </animated.div>
     </Link>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,40 +19,40 @@ class Home extends Component {
               title={'Illini Events'}
               link={'/illini-events'}
               img={'../../assets/illini_events.jpg'}
-              backgroundColor='#292929'
-              description='Created a mobile app to help advertisers increase traffic to their events and to help students find events they may be interested in around campus.'
+              backgroundColor='rgba(229, 70, 31, 0.5)'
+              description='College event finder'
               tags={['JavaScript', 'React Native', 'Redux']}
             />
             <Card
               title={"Emmy's Doodles"}
               link={'/emmys-doodles'}
               img={'../../assets/emmys_doodles.jpg'}
-              backgroundColor='#292929'
-              description=''
+              backgroundColor='rgba(244, 219, 155, 0.5)'
+              description='E-commerce website for goldendoodle stickers'
               tags={['JavaScript', 'React', 'Redux', 'Firebase']}
             />
             <Card
               title={'Browser Game'}
               link={'/browser-game'}
               img={'../../assets/browser_game.jpg'}
-              backgroundColor='#292929'
-              description='Developed a survival/wave game with a friend in pure JavaScript; the goal of the game is to gain as many points as possible by defeating enemies and chaining combos. The intent of this project was to learn how to program a game. Since neither of us are artists, the visuals were drawn in GIMP and are placeholder.'
+              backgroundColor='rgba(150, 191, 206, 0.5)'
+              description='Browser-based survival game'
               tags={['JavaScript', 'HTML5', 'Canvas']}
             />
             <Card
               title={'Message in a Bottle'}
               link={'/message-in-a-bottle'}
               img={'../../assets/message_in_a_bottle.jpg'}
-              backgroundColor='#292929'
-              description='Created a mobile app with a group of five students. The app allows users to set goals, vent their frustrations throughout the process, and finally motivates users to complete these goals with motivational quotes. I worked on the front-end of this app with two other students.'
+              backgroundColor='rgba(150, 206, 180, 0.5)'
+              description='Motivational task management'
               tags={['JavaScript', 'React Native', 'Node.js', 'Express', 'MongoDB']}
             />
             <Card
               title={'Fanchat'}
               link={'/fanchat'}
               img={'../../assets/fanchat.jpg'}
-              backgroundColor='#292929'
-              description='WIP'
+              backgroundColor='rgba(255, 32, 32, 0.5)'
+              description='Subscription-based chatting'
               tags={['Javascript', 'React Native Web', 'Firebase', 'Redux']}
             />
           </section>
