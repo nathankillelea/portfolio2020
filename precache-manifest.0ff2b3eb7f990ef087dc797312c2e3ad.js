3a22ede9c3873419f91d09539f449949de0ce77b
nathankillelea <nathan.killelea@gmail.com>
2020-11-11 03:14:59 -0600

    Updates

---
+++ precache-manifest.0ff2b3eb7f990ef087dc797312c2e3ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60263e242394151075eec3dd718dee32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2d937ed50f07cf40931",
+    "url": "/static/css/main.4fb30403.chunk.css"
+  },
+  {
+    "revision": "653f03f8cf3987365682",
+    "url": "/static/js/2.a25a2482.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a25a2482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d937ed50f07cf40931",
+    "url": "/static/js/main.7fa4d719.chunk.js"
+  },
+  {
+    "revision": "57c7a1937fd6dacf4e96",
+    "url": "/static/js/runtime-main.d18c2d5e.js"
+  }
+]);
